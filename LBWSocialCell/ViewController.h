aabd0a229cd8404d688009e4fe28b540dacd29d6
leeB0Wen <ml@mldemac-mini.lan>
2016-07-22 15:40:19 +0800

    Initial Commit

---
+++ LBWSocialCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBWSocialCell
+//
+//  Created by ml on 16/7/22.
+//  Copyright © 2016年 李博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LBWSocialCell/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LBWSocialCell
+//
+//  Created by ml on 16/7/22.
+//  Copyright © 2016年 李博文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
